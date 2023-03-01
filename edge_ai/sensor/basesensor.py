6fd1a11258493b08759d0f154663cddd316a798a
Aldrich Suratos <aldrich.suratos@ksk-anl.com>
2023-03-01 15:51:37 +0900

    moved common sensor functions to base class

--- edge_ai/sensor/basesensor.py
+++ edge_ai/sensor/basesensor.py
@@ -1,23 +1,46 @@
+import multiprocessing as mp
+
+from typing import Type, Tuple
 from abc import ABC, abstractmethod
+from multiprocessing.connection import Connection
+
+from ..bus import BaseBus
 
 class BaseSensor(ABC):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, bus: Type[BaseBus]) -> None:
+        self.pipe_external, self.pipe_internal = mp.Pipe(True)
+        self._bus = bus
     
     @property
-    @abstractmethod
-    def _process(self):
-        return self._process
-        ...
-    
-    @abstractmethod
-    def start(self):
-        ...
-    
-    @abstractmethod
-    def stop(self):
-        ...
+    def _internal_pipe(self) -> Connection:
+        return self.pipe_internal
         
-    @abstractmethod
+    @property
+    def _external_pipe(self) -> Connection:
+        return self.pipe_external
+
+    @property
+    def _bus(self) -> Type[BaseBus]:
+        return self._bus
+        
+    @property
+    def _process(self) -> mp.Process:
+        return self._process
+    
+    def start(self):
+        self._process = mp.Process(target = self.__internal_loop, 
+                                   args = (self._internal_pipe, ))
+        self._process.start()
+    
+    def stop(self):
+        # close running process
+        self._process.kill()
+        
     def read(self):
+        self._external_pipe.send("read")
+        
+        return self._external_pipe.recv()
+
+    @abstractmethod
+    def __internal_loop(self):
         ...
--- edge_ai/sensor/lis3dh.py
+++ edge_ai/sensor/lis3dh.py
@@ -1,32 +1,15 @@
 import multiprocessing as mp
 
+from typing import Type
 from multiprocessing.connection import Connection
 
 from . import BaseSensor
 from ..bus import BaseBus
 
 class LIS3DH(BaseSensor):
-    def __init__(self, bus) -> None:
-        super().__init__()
-        
-        assert issubclass(bus, BaseBus)
-        self.bus = bus
-        self.pipe_external, self.pipe_internal = mp.Pipe(True)
-
-    def read(self):
-        # send "read" message to the internal process (down the pipe)
-        
-        # read from the pipe and return
-        pass
+    def __init__(self, bus: Type[BaseBus]) -> None:
+        super().__init__(bus)
     
-    def start(self):
-        # run an internal process that can be controlled via pipe message communication
-        pass
-    
-    def stop(self):
-        # close running process
-        pass
-
     def __internal_loop(self, pipe: Connection):
         # this is a loop that manages the running of the sensor.
         latest_value = None
