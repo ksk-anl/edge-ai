7d315dbfd1c0c8c214b788d47b70ccd9ff1ff373
Aldrich Suratos <aldrich.suratos@ksk-anl.com>
2023-03-09 17:26:49 +0900

    moved constants to a json file

--- record_and_send.py
+++ record_and_send.py
@@ -1,6 +1,7 @@
 import io
 import time
 import math
+import json
 import datetime
 
 import psycopg2
@@ -12,22 +13,33 @@ from edge_ai.controller.adc import ADS1015
 TIMEFORMAT = '%Y-%m-%d %H:%M:%S.%f'
 
 # Postgres DB Access information
-RDBACCESS = {
-    'dbname': 'edge',
-    'user'  :   'postgres',
-    'password': 'postgres',
-    'host': '192.168.0.38',
-    'port': '5432'
-}
-DEVICE_ID = 1
+# RDBACCESS = {
+#     'dbname': 'edge',
+#     'user'  :   'postgres',
+#     'password': 'postgres',
+#     'host': '192.168.0.38',
+#     'port': '5432'
+# }
+with open("config.json") as f:
+    config = json.loads(f)
+    RDBACCESS = config['rdb_access']
 
-ADC_THRESHOLD = 2.5
-NUMBER_OF_MEASUREMENTS = 20
+    DEVICE_ID = config['device_id']
+
+    ADC_THRESHOLD = config['adc_threshold']
+    NUMBER_OF_MEASUREMENTS = config['measurements']
+
+    MOTIONSENSOR = config['motionsensor_spi']
+    ADC = config['adc_i2c']
+
+    MEASUREMENT_WINDOW = config['window_length']
+    WAIT_TIME = config['wait_time']
+    ADC_MEASUREMENT_INTERVAL = config['adc_measurement_interval']
 
 def main():
 
-    motionsensor = LIS3DH.SPI(busnum = 0, cs = 0)
-    adc = ADS1015.I2C(address = 0x48, busnum = 1)
+    motionsensor = LIS3DH.SPI(**MOTIONSENSOR)
+    adc = ADS1015.I2C(**ADC)
 
     # Setup sensors
     motionsensor.set_datarate(5376)
@@ -36,29 +48,27 @@ def main():
 
     adc.start()
 
-    delta_t = 3
-
     conn = psycopg2.connect(**RDBACCESS)
 
     for _ in range(NUMBER_OF_MEASUREMENTS):
         print('Waiting for blockage...')
 
         while True:
-            time.sleep(0.1)
+            time.sleep(ADC_MEASUREMENT_INTERVAL)
             val = adc.read()
             if val > ADC_THRESHOLD:
                 break
 
-        time.sleep(0.25)
+        time.sleep(WAIT_TIME)
 
         results = []
 
-        time_end = time.time() + delta_t
+        time_end = time.time() + MEASUREMENT_WINDOW
         while time.time() <= time_end:
             values = motionsensor.read()
-            final_value = math.sqrt(sum([x**2 for x in values]))
+            final_value = math.sqrt(sum([x ** 2 for x in values]))
 
-            results.append([f'{datetime.datetime.now():{TIMEFORMAT}}',final_value])
+            results.append([f'{datetime.datetime.now():{TIMEFORMAT}}', final_value])
 
         print(f'Recorded {len(results)} lines!')
 
