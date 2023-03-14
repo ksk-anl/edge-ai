e7ef78f53168ea875ed3c4a148a3eb5c6d63d35d
Aldrich Suratos <aldrich.suratos@ksk-anl.com>
2023-03-14 16:17:50 +0900

    adding features to runner.py to allow it to start and stop the daemon

--- runner.py
+++ runner.py
@@ -1,5 +1,7 @@
 import os
 import daemon
+import argparse
+
 import record_and_send
 
 def main():
@@ -11,5 +13,27 @@ def main():
     with context:
         record_and_send.main()
 
+def run_infinite():
+    context = daemon.DaemonContext()
+
+    with context:
+        while True:
+            pass
+
+def stop():
+    os.system('pkill -x "python3 runner"')
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('action',
+                        choices = ['start', 'stop', 'test'],
+                        required = True,
+                        help = '"start" or "stop" the measurement script')
+    args = parser.parse_args()
+
+    if args.action == 'start':
+        main()
+    elif args.action == 'stop':
+        stop()
+    elif args.action == 'test':
+        run_infinite()
